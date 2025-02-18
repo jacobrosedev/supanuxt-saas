4d3b6fb9f79d7be63de1048fa8137998ae5b8ab9
Jacob Rose <jacobrosecode@gmail.com>
2025-02-18 12:14:48 -0500

    renamed admin dashboard.vue to index.vue

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,4 +1,22 @@
-<template>
-    <div>Admin /admin Route... nothing to see here.</div>
+<script setup lang="ts">
+  definePageMeta({
+    middleware: ['auth']
+  });
 
+  // useRoles Composable for RBAC
+  const { isAdmin, isWorker, isTenant } = useRoles();
+
+</script>
+<template>
+  <div
+    class="flex flex-col items-center max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+    <div class="text-center mb-12">
+      <h2 v-if="isTenant"
+        class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mb-4">
+        Admin Dashboard
+      </h2>
+    </div>
+
+    
+  </div>
 </template>
