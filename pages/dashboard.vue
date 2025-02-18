17ed2dd33488d34606a6a597dfe7011059965af0
Jacob Rose <jacobrosecode@gmail.com>
2025-02-18 12:15:58 -0500

    setup /dashboard shorthand route with rbac to auto-route user to dashboard based on role /admin/dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,22 +1,11 @@
 <script setup lang="ts">
-  definePageMeta({
-    middleware: ['auth']
-  });
-
-  // useRoles Composable for RBAC
-  const { isAdmin, isWorker, isTenant } = useRoles();
-
+definePageMeta({
+    middleware: ['rbac']
+});
 </script>
-<template>
-  <div
-    class="flex flex-col items-center max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-    <div class="text-center mb-12">
-      <h2 v-if="isTenant"
-        class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mb-4">
-        Tenant Dashboard
-      </h2>
-    </div>
 
-    
-  </div>
+<template>
+    <div>
+        <h2>RBAC Transfer</h2>
+    </div>
 </template>
