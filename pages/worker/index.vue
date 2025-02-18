f96f399d356ffd5d4c4971a73d30c2abce2303f9
Jacob Rose <jacobrosecode@gmail.com>
2025-02-18 12:15:11 -0500

    renamed worker dashboard.vue to index.vue

--- pages/worker/index.vue
+++ pages/worker/index.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+  definePageMeta({
+    middleware: ['auth']
+  });
+
+  // useRoles Composable for RBAC
+  const { isAdmin, isWorker, isTenant } = useRoles();
+
+</script>
 <template>
-    <div>Worker Home</div>
+  <div
+    class="flex flex-col items-center max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+    <div class="text-center mb-12">
+      <h2 v-if="isTenant"
+        class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mb-4">
+        Worker Dashboard
+      </h2>
+    </div>
+
+    
+  </div>
 </template>
