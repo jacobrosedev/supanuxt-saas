61623fb975736ace79c9f58841f90c8ead456b3a
Jacob Rose <jacobrosecode@gmail.com>
2025-02-21 01:53:37 -0500

    created pages for admin tools charges and payments

---
+++ pages/admin/charges.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// Admin can see all users with toggles for Applicants, Tenants, Workers, and Admins.
+// There is a search bar at the top, where you can search phone, email, and name.
+// Units, Users, Leases
+
+</script>
+
+<template>
+    <div>
+        <h2>Admin Charges Table</h2>
+        <p>all rent charges displayed as balance remaining for each tenant per unit</p>
+    </div>
+</template>
---
+++ pages/admin/payments.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// Admin can see all payments
+</script>
+
+<template>
+    <div>
+        <h2>Admin Payments Table</h2>
+        <p></p>
+    </div>
+</template>
