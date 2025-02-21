4b38727f45cd51cc0de405ee3553a209920e9ac0
Jacob Rose <jacobrosecode@gmail.com>
2025-02-21 18:32:59 -0500

    basic tenant and worker onboarding flow

--- pages/apply/tenant.vue
+++ pages/apply/tenant.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="prose lg:prose-xl m-5">
       <p>Tenant Application</p>
+      <p>address search bar</p>
+      <p>available rentals near you</p>
     </div>
   </template>
---
+++ pages/apply/worker.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="prose lg:prose-xl m-5">
+        <p>Worker Application</p>
+        <p>Worker Scheduling</p>
+        <p>Worker Expertise </p>
+    </div>
+  </template>
