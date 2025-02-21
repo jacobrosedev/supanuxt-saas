7b1d51d9982c4e5f4ef9584f7aff0b2a2c42557d
Jacob Rose <jacobrosecode@gmail.com>
2025-02-21 09:28:57 -0500

    moved to public

--- pages/forgot.vue
+++ pages/forgot.vue
@@ -12,7 +12,7 @@
       const { data, error } = await supabase.auth.resetPasswordForEmail(
         email.value,
         {
-          redirectTo: `${config.public.siteRootUrl}/u/resetpassword`
+          redirectTo: `${config.public.siteRootUrl}/auth/reset`
         }
       );
       if (error) throw error;
