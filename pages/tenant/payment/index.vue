917ba45b67c3a622c57d9f44d3c44ce78f1ca213
Jacob Rose <jacobrosecode@gmail.com>
2025-02-25 06:41:40 -0500

    commented out some code so it loads

--- pages/tenant/payment/index.vue
+++ pages/tenant/payment/index.vue
@@ -5,31 +5,31 @@
 
 
 
-import {loadStripe} from '@stripe/stripe-js';
-const config = useRuntimeConfig();
-const stripe = await loadStripe(config.public.stripePublicTest);
-const { data: product } = await useFetch('/server/api/get-product')
+// import {loadStripe} from '@stripe/stripe-js';
+// const config = useRuntimeConfig();
+// const stripe = await loadStripe(config.public.stripePublicTest);
+// // const { data: product } = await useFetch('/server/api/get-product')
 
 
 
 const handlePayment = async () => {
- const { data } = await useFetch('/server/api/create-session', {
-   method: 'POST',
-   body: { priceId: product.value.default_price }
- })
+//  const { data } = await useFetch('/server/api/create-session', {
+//    method: 'POST',
+//    body: { priceId: product.value.default_price }
+//  })
  
- await stripe.redirectToCheckout({
-   sessionId: data.sessionId
- })
+//  await stripe.redirectToCheckout({
+//    sessionId: data.sessionId
+//  })
 }
 </script>
 
 <template>
   <div> <h2>Tenant Rent Payment</h2>
     <div v-if="product" class="p-6 max-w-md mx-auto">
-      
+<!--       
       <h2 class="text-xl font-bold">{{ product.name }}</h2>
-      <p class="mt-2">${{ product.default_price.unit_amount / 100 }}</p>
+      <p class="mt-2">${{ product.default_price.unit_amount / 100 }}</p> -->
       <button @click="handlePayment" class="mt-4 w-full bg-blue-500 text-white p-2 rounded"
       >Pay Now</button>
     </div>
