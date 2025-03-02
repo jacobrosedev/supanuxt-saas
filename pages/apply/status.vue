9193ef95ab9875835c1fa8ce16976ffc8929a814
Jacob Rose <jacobrosecode@gmail.com>
2025-03-02 13:13:04 -0500

    skeleton

--- pages/apply/status.vue
+++ pages/apply/status.vue
@@ -9,6 +9,15 @@
         class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl mb-4">
         Tenant | Worker Application Status
       </h2>
+      <p>Your application is being reviewed. Please wait for admin approval.</p>
+      <div class="block flex w-full border">
+        <button class="border">
+          verify
+        </button>
+        <button class="border">
+          review
+        </button>
+      </div>
     </div>
 
   </div>
