e66285803dfec5e1503be237ddfa4f1ad1ff8dd8
Jacob Rose <jacobrosecode@gmail.com>
2025-03-29 11:26:40 -0400

    each input type only has one data value to track, so we use defineModel() to bind child components to the parent page's data values

---
+++ components/Input/InputDate.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useDOB } from '~/composables/formatters.js';
+const model = defineModel()
+const dobFormat = computed(() => useDOB(model.value))
+const updateDob = (event) => {
+  model.value = event.target.value.replace(/\D/g, '')
+}
+</script>
+
+<template>
+  <div  class="form-control">
+    <label for="dob" class="block mb-2 font-bold">Date of birth:</label>
+    <input class="w-[124px] p-2 border border-gray-400 rounded-md"
+      placeholder="MM/DD/YYYY" maxlength="10" id="dob" type="text"
+      @input="updateDob" :value="dobFormat" required
+    />
+  </div>
+</template>
---
+++ components/Input/InputPhone.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { usePhone } from '~/composables/formatters.js';
+const model = defineModel()
+const phoneFormat = computed(() => usePhone(model.value))
+const updatePhone = (event) => {
+  model.value = event.target.value.replace(/\D/g, '')
+}
+</script>
+
+<template>
+  <div class="form-control">
+    <label for="phone" class="block mb-2 font-bold">Phone:</label>
+    <input class="w-[128px] p-2 border border-gray-400 rounded-md"
+      placeholder="(321) 654-0987" maxlength="14" id="phone" type="phone"
+      @input="updatePhone" :value="phoneFormat" required
+    />
+  </div>
+</template>
